08e9ec8995d42cc63fbdbdcb43204c1c531b055a
Grzegorz Durtan <grzegorz.durtan@benedeo.com>
2018-03-31 01:38:57 +0200

    files

--- vue-2-boilerplate/src/routes/index.js
+++ vue-2-boilerplate/src/routes/index.js
@@ -6,41 +6,6 @@
  */
 
 export default [
-  // Home
-  {
-    path: '/home',
-    name: 'home.index',
-    component: () => import('@/pages/Home/Index'),
-
-    // If the user needs to be authenticated to view this page
-    meta: {
-      auth: true,
-    },
-  },
-
-  // Account
-  {
-    path: '/account',
-    name: 'account.index',
-    component: () => import('@/pages/Account/Index'),
-
-    // If the user needs to be authenticated to view this page.
-    meta: {
-      auth: true,
-    },
-  },
-
-  // Login
-  {
-    path: '/login',
-    name: 'login.index',
-    component: () => import('@/pages/Login/Index'),
-
-    // If the user needs to be a guest to view this page.
-    meta: {
-      guest: true,
-    },
-  },
 
   // Facebook Login
   {
@@ -54,25 +19,4 @@ export default [
     },
   },
 
-  // Register
-  {
-    path: '/register',
-    name: 'register.index',
-    component: () => import('@/pages/Register/Index.vue'),
-
-    // If the user needs to be a guest to view this page.
-    meta: {
-      guest: true,
-    },
-  },
-
-  {
-    path: '/',
-    redirect: '/home',
-  },
-
-  {
-    path: '/*',
-    redirect: '/home',
-  },
 ];
